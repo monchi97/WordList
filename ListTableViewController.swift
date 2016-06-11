b3e4150f975f3a13180f9f9a9e10f03bd696bfed
Kenta Okuda <kenta19940520@gmail.com>
2016-06-12 00:38:05 +0900

    fix: modify some issues.

--- ListTableViewController.swift
+++ ListTableViewController.swift
@@ -26,13 +26,17 @@ class ListTableViewController: UITableViewController {
 
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
         if saveData.arrayForKey("WORD") != nil {
             wordArray = saveData.arrayForKey("WORD")!
         }
         tableView.reloadData()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
 
     // MARK: - Table view data source
